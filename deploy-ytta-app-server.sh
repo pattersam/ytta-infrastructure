787297fd8e81c39979adf5ef2958ca7c3e89fa00
Sam <spatterson380@gmail.com>
2022-04-08 14:03:05 +0000

    adding extra env variables
    required for celery worker & boto

--- deploy-ytta-app-server.sh
+++ deploy-ytta-app-server.sh
@@ -2,4 +2,8 @@ sed -e "s|FIRST_SUPERUSER_VALUE|$FIRST_SUPERUSER|g" aws-cloudformation/ytta-app-
 sed -i -e "s|FIRST_SUPERUSER_PASSWORD_VALUE|$FIRST_SUPERUSER_PASSWORD|g" ytta-app-backend-deployment-with-secrets.yaml
 sed -i -e "s|POSTGRES_SERVER_VALUE|$POSTGRES_SERVER|g" ytta-app-backend-deployment-with-secrets.yaml
 sed -i -e "s|POSTGRES_PASSWORD_VALUE|$POSTGRES_PASSWORD|g" ytta-app-backend-deployment-with-secrets.yaml
+sed -i -e "s|AWS_ACCESS_KEY_ID_VALUE|$AWS_ACCESS_KEY_ID|g" ytta-app-backend-deployment-with-secrets.yaml
+sed -i -e "s|AWS_SECRET_ACCESS_KEY_VALUE|$AWS_SECRET_ACCESS_KEY|g" ytta-app-backend-deployment-with-secrets.yaml
+sed -i -e "s|AWS_DEFAULT_REGION_VALUE|$AWS_DEFAULT_REGION|g" ytta-app-backend-deployment-with-secrets.yaml
+sed -i -e "s|SQS_URL_VALUE|$SQS_URL|g" ytta-app-backend-deployment-with-secrets.yaml
 kubectl apply -f ytta-app-backend-deployment-with-secrets.yaml
